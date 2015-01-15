14246ffc4016344babb4a40f59f0d969cec1e00b
Max Kuang <sk4364@nyu.edu>
2015-01-15 16:02:46 -0500

    cfr folder

---
+++ study/cfr/traverse_tree/Gamestat.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 14 14:11:36 2015
+
+@author: max
+"""
+
+class Gamestat:
+    def __init__(self):
+        self.num_preflop_state = 0
+        self.num_preflop_state_D = 0
+        self.num_preflop_state_SB = 0
+        self.num_preflop_state_BB = 0
+        self.num_flop_state = 0
+        self.num_flop_state_D = 0
+        self.num_flop_state_SB = 0
+        self.num_flop_state_BB = 0 
+        self.num_turn_state = 0
+        self.num_turn_state_D = 0
+        self.num_turn_state_SB = 0
+        self.num_turn_state_BB = 0
+        self.num_river_state = 0
+        self.num_river_state_D = 0
+        self.num_river_state_SB = 0
+        self.num_river_state_BB = 0
+        self.num_end_state = 0
+    def set_zero(self):
+        self.num_preflop_state = 0
+        self.num_preflop_state_D = 0
+        self.num_preflop_state_SB = 0
+        self.num_preflop_state_BB = 0
+        self.num_flop_state = 0
+        self.num_flop_state_D = 0
+        self.num_flop_state_SB = 0
+        self.num_flop_state_BB = 0 
+        self.num_turn_state = 0
+        self.num_turn_state_D = 0
+        self.num_turn_state_SB = 0
+        self.num_turn_state_BB = 0
+        self.num_river_state = 0
+        self.num_river_state_D = 0
+        self.num_river_state_SB = 0
+        self.num_river_state_BB = 0
+        self.num_end_state = 0
+            
