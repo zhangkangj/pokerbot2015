f66cade03d3bbd1f280ef7021f04a416217a9b5a
NYUShawn <ideal.arthur.li@gmail.com>
2015-01-16 18:59:23 -0800

    query

--- analysis/Query.py
+++ analysis/Query.py
@@ -26,11 +26,16 @@ def main():
   FILENAME = "Day2.csv"
   FILEPATH = LOGPATH + "\\" + FILENAME
   df = pd.read_csv(FILEPATH)
-  preflop(df, LOGPATH)
+ # df['num_of_player_preflop'] = np.sum(df['is_dealer_enter_preflop'], df['is_SB_enter_preflop'], df['is_BB_enter_preflop'])
+  df['num_of_player_preflop'] = np.sum(df[['is_dealer_enter_preflop','is_SB_enter_preflop','is_BB_enter_preflop']])    
+  df['actual_dealer_equity_preflop'] = (3-df['num_of_player_preflop'])*df['dealer_equity_2']+(df['num_of_player_preflop']-2)*df['dealer_equity_3']
+  df['num_of_player_preflop'] = df['is_dealer_enter_flop'] + df['is_SB_enter_flop'] + df['is_BB_enter_flop']  
+  #preflop(df, LOGPATH)
   
 def preflop(df, LogPath):
   Filename = "Preflop Analysis.csv"
   OutputPath = LogPath + "\\" + Filename
+  
   df_dealer = df[['dealer_name','dealer_hole_cards','dealer_equity_3','dealer_actions_preflop']]
   df_sb = df[["SB_name","SB_hole_cards","SB_equity_3","SB_actions_preflop"]]
   df_bb = df[["BB_name","BB_hole_cards","BB_equity_3","BB_actions_preflop"]]
@@ -41,8 +46,20 @@ def preflop(df, LogPath):
   preflop = df_dealer.append(df_sb.append(df_bb))
   preflop = preflop.sort_index(by=["Name","Equity"], ascending = [0,0])
   preflop.to_csv(OutputPath)
+
+def AfterPreFlop(df, LogPath):
+  Filename = "AfterPreFlop Analysis.csv"
+  OutputPath = LogPath + '\\' + Filename
+  df = df[df.PreFlop_potsize != 'null']
+  df_dealer = df[df.is_dealer_enter_flop]
+  df_sb = df[df.is_SB_enter_flop]  
+  df_bb = df[df.is_BB_enter_flop]  
+  df_dealer['Position'] = 'D'
+  df_sb['Position'] = 'SB'
+  df_bb['Position'] = 'BB'
+  df_dealer = df_dealer[['dealer_name',]]
   
-    
+   
   
     
 if __name__=="__main__":
