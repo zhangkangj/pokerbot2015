0621e948d532bfad4138e87f31c6e18d6b69721b
zhangkangj <zhangkangj@gmail.com>
2015-01-19 00:44:13 -0500

    kuhn

--- lib/evaluator/evaluator.py
+++ lib/evaluator/evaluator.py
@@ -8,7 +8,13 @@ Created on Sat Jan 10 02:24:21 2015
 import numpy as np
 
 from lib import util
-from lib.evaluator import pbots_calc, evaluator_cy
+from lib.evaluator import pbots_calc
+try:
+  from lib.evaluator import evaluator_cy
+except:
+  import traceback
+  traceback.print_exc()
+  print 'failed to import evaluator_cy'
 
 def evaluate(player_cards, board, dead, num_iter=1000):
   result = pbots_calc.calc(player_cards, board, dead, num_iter)
--- study/cfr/cfr_kuhn.py
+++ study/cfr/cfr_kuhn.py
@@ -10,16 +10,59 @@ np.set_printoptions(precision=3, suppress=True)
 import pyximport
 pyximport.install(setup_args={'include_dirs': [np.get_include(), 'lib/evaluator']}, reload_support=True, inplace=True)
 from study.cfr import cfr_cy
-
 import itertools
 
+
 reload(cfr_cy)
+
 root = cfr_cy.RoundNode(3, 2, 1, 1)
 seq1 = np.array([0, 0, 0, 0, 0])
 seq2 = np.array([0, 0, 0, 0, 0])
 
+seq1[3] = seq1[4] = 1
+seq2[3] = seq2[4] = 1
+util_sb_, util_bb_ = root.run_cfr(seq1, seq2)
+print 'sb: root              ', root.child_nodes[0].regret[0:6]
+print 'bb: sb check          ', root.child_nodes[0].child_nodes[0].regret[0:6]
+print 'bb: sb raise          ', root.child_nodes[0].child_nodes[1].regret[0:6]
+print 'sb: sb check, bb raise', root.child_nodes[0].child_nodes[0].child_nodes[1].regret[0:6]
+print util_sb_, util_bb_
 
-for i in range(1000000):
+seq1[3] = seq1[4] = 1
+seq2[3] = seq2[4] = 1
+util_sb_, util_bb_ = root.run_cfr(seq1, seq2)
+print 'sb: root              ', root.child_nodes[0].regret[0:6]
+print 'bb: sb check          ', root.child_nodes[0].child_nodes[0].regret[0:6]
+print 'bb: sb raise          ', root.child_nodes[0].child_nodes[1].regret[0:6]
+print 'sb: sb check, bb raise', root.child_nodes[0].child_nodes[0].child_nodes[1].regret[0:6]
+print util_sb_, util_bb_
+
+seq1[3] = seq1[4] = 1
+seq2[3] = seq2[4] = 2
+util_sb_, util_bb_ = root.run_cfr(seq1, seq2)
+print 'sb: root              ', root.child_nodes[0].regret[0:6]
+print 'bb: sb check          ', root.child_nodes[0].child_nodes[0].regret[0:6]
+print 'bb: sb raise          ', root.child_nodes[0].child_nodes[1].regret[0:6]
+print 'sb: sb check, bb raise', root.child_nodes[0].child_nodes[0].child_nodes[1].regret[0:6]
+print util_sb_, util_bb_
+
+
+
+
+
+def normalize(array):
+  array = np.clip(array, 0, np.inf) + 1e-10
+  coeff = np.empty(array.size)
+  coeff[::2] = array[::2] + array[1::2]
+  coeff[1::2] = coeff[0::2]
+  return array/coeff
+
+reload(cfr_cy)
+
+root = cfr_cy.RoundNode(3, 2, 1, 1)
+seq1 = np.array([0, 0, 0, 0, 0])
+seq2 = np.array([0, 0, 0, 0, 0])
+for i in range(100000):
   util_sb = util_bb = 0.0
   for sb, bb, _ in itertools.permutations([0,1,2]):
    seq1[3] = seq1[4] = sb
@@ -27,9 +70,12 @@ for i in range(1000000):
    util_sb_, util_bb_ = root.run_cfr(seq1, seq2)
    util_sb += util_sb_
    util_bb += util_bb_
-  print i, util_sb/6, util_bb/6
+  if i%1000 == 0:
+    print 'sb: root              ', normalize(root.child_nodes[0].regret[0:6])
+    print 'bb: sb check          ', normalize(root.child_nodes[0].child_nodes[0].regret[0:6])
+    print 'bb: sb raise          ', normalize(root.child_nodes[0].child_nodes[1].regret[0:6])
+    print 'sb: sb check, bb raise', normalize(root.child_nodes[0].child_nodes[0].regret[0:6])
+    print i, util_sb/6, util_bb/6
+    print
+  
 
-print 'sb: root              ', root.child_nodes[0].regret[0:3]
-print 'bb: sb check          ', root.child_nodes[0].child_nodes[0].regret[0:3]
-print 'bb: sb raise          ', root.child_nodes[0].child_nodes[1].regret[0:3]
-print 'sb: sb check, bb raise', root.child_nodes[0].child_nodes[0].regret[0:3]
