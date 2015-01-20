1dfb24381f87866db3ef587318eb861a537ed3e3
jameswww <james.wang.ccc@gmail.com>
2015-01-20 13:08:36 -0500

    enhanced add bot script to build your bot from any exisitng bot

--- bot/mixedoppnew/mixedoppnew_bot.py
+++ bot/mixedoppnew/mixedoppnew_bot.py
@@ -17,7 +17,7 @@ class MixedoppnewBot(base_bot.BaseBot):
     hole_card_str = ''.join(self.player.hole_cards)
     board_card_str = ''.join(self.player.board_cards)
     card_str = hole_card_str+':xx'*(self.player.num_active_player-1)
-    equity = evaluator.evaluate(card_str, board_card_str, '', 100)
+    equity = evaluator.evaluate(card_str, board_card_str, '', 500)
     can_raise = False
     can_bet = False
     can_call = False
