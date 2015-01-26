64ffc18d1fad85ae12d8a0f765c4ea2f76d17d5d
zhangkangj <zhangkangj@gmail.com>
2015-01-25 23:02:10 -0500

    cfr nodes changed

--- study/cfr/cfr_main2.py
+++ study/cfr/cfr_main2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 16 15:37:03 2015
+Created on Sun Jan 25 16:29:51 2015
 
 @author: zhk
 """
@@ -9,10 +9,10 @@ import numpy as np
 np.set_printoptions(precision=3, suppress=True)
 import pyximport
 pyximport.install(setup_args={'include_dirs': [np.get_include(), 'lib/evaluator']}, reload_support=True, inplace=True)
-from study.cfr import cfr_cy2
 from lib.evaluator import evaluator_cy, evaluator
 from lib import util
-
+from study.cfr import cfr_cy2, cfr_cy3, cfr_cy4
+import time
 
 def normalize(array, n):
   array = np.clip(array, 0, np.inf) + 1e-10
@@ -25,62 +25,14 @@ def normalize(array, n):
   return array/coeff
 
 reload(cfr_cy2)
-
 root = cfr_cy2.RoundNode(0, 0, 4, 4)
 root.initialize_regret()
 seq1 = np.array([0, 0, 0, 0, 0])
 seq2 = np.array([0, 0, 0, 0, 0])
 util_sb = util_bb = 0
-for i in range(1,10000):
-  mc1 = 20
-  mc2 = 21
-  #oc1 = 1
-  #oc2 = 19
-  #bc1, bc2, bc3, bc4, bc5 = np.random.choice([x for x in range(0,52) if x!=mc1 and x!=mc2 and x!=oc1 and x!=oc2], 5, replace=False)
-  oc1, oc2, bc1, bc2, bc3, bc4, bc5 = np.random.choice([x for x in range(0,52) if x!=mc1 and x!=mc2], 7, replace=False)
-  seq1[0] = evaluator_cy.preflop_idx(mc1, mc2)
-  seq2[0] = evaluator_cy.preflop_idx(oc1, oc2)
-  seq1[1], _, _  = evaluator.evaluate_flop(mc1, mc2, bc1, bc2, bc3)
-  seq2[1], _, _  = evaluator.evaluate_flop(oc1, oc2, bc1, bc2, bc3)
-  seq1[2], _, _  = evaluator.evaluate_turn(mc1, mc2, bc1, bc2, bc3, bc4)
-  seq2[2], _, _  = evaluator.evaluate_turn(oc1, oc2, bc1, bc2, bc3, bc4)
-  seq1[3], _ = evaluator.evaluate_river(mc1, mc2, bc1, bc2, bc3, bc4, bc5)
-  seq2[3], _ = evaluator.evaluate_river(oc1, oc2, bc1, bc2, bc3, bc4, bc5)
-  seq1[4] = evaluator.evaluate_cards(mc1, mc2, bc1, bc2, bc3, bc4, bc4)
-  seq2[4] = evaluator.evaluate_cards(oc1, oc2, bc1, bc2, bc3, bc4, bc4)
-  util_sb_, util_bb_ = root.run_cfr(seq1, seq2)
-  util_sb += util_sb_
-  util_bb += util_bb_
-  if i%100 == 0:
-    print i, util_sb/i, util_bb/i
-    print 'sb: root    ', np.reshape(root.child_nodes[0].average_prob[:507], (169,3))[evaluator_cy.preflop_idx(mc1, mc2)]
-    print 'bb: sb raise', np.reshape(root.child_nodes[0].child_nodes[2].average_prob[:338], (169,2))[evaluator_cy.preflop_idx(0, 21)]
-    print 'bb: sb raise', np.reshape(root.child_nodes[0].child_nodes[2].average_prob[:338], (169,2))[evaluator_cy.preflop_idx(mc1, mc2)]    
-    print 'bb: sb raise', np.reshape(root.child_nodes[0].child_nodes[2].average_prob[:338], (169,2))[evaluator_cy.preflop_idx(48, 49)]
-    print    
 
-# fix cards
-print 'sb: root    ', root.child_nodes[0].average_prob[30:33]
-print 'bb: sb check', root.child_nodes[0].child_nodes[1].average_prob[:338]
-print 'bb: sb raise', np.reshape(root.child_nodes[0].child_nodes[2].average_prob[:338], (169,2))
-bb_prob = np.reshape(root.child_nodes[0].child_nodes[2].average_prob[:338], (169,2))
-bb_regret = np.reshape(root.child_nodes[0].child_nodes[2].regret[:338], (169,2))
-for i in range(51):
-  for j in range(i+1, 52):
-    idx = evaluator_cy.preflop_idx(i, j)
-    print util.n2c([i,j]), idx, bb_prob[idx]#, bb_regret[idx]
-
-
-
-
-
-
-root = cfr_cy2.RoundNode(0, 0, 300, 300)
-root.initialize_regret()
-seq1 = np.array([0, 0, 0, 0, 0])
-seq2 = np.array([0, 0, 0, 0, 0])
-util_sb = util_bb = 0
-for i in range(1,100):
+start_time = time.time()
+for i in range(1,100000):
   mc1, mc2, oc1, oc2, bc1, bc2, bc3, bc4, bc5 = np.random.choice(52, 9, replace=False)
   seq1[0] = evaluator_cy.preflop_idx(mc1, mc2)
   seq2[0] = evaluator_cy.preflop_idx(oc1, oc2)
@@ -95,8 +47,9 @@ for i in range(1,100):
   util_sb_, util_bb_ = root.run_cfr(seq1, seq2)
   util_sb += util_sb_
   util_bb += util_bb_
-  if i%10 == 0:
+  if time.time() - start_time > 0.1:
     print i, util_sb/i, util_bb/i
+    start_time = time.time()
 
 n = 3
 result = np.reshape(normalize(root.child_nodes[0].average_prob[:(n*169)], n=n), (169,n))
@@ -106,28 +59,44 @@ for i in range(51):
     print util.n2c([i,j]), idx, result[idx]
 
 
-
-
-
-
-
-
-root = cfr_cy2.RoundNode(0, 0, 300, 300)
+reload(cfr_cy4)
+root = cfr_cy4.RoundNode(0, 0, 300, 300)
 root.initialize_regret()
-root.load_regret('data/cfr/aws/regret_300_total.npy')
-root.load_prob('data/cfr/aws/prob_300_total.npy')
-node = root.child_nodes[0]
-num_child = 4
-prob = np.reshape(node.average_prob[:(169*num_child)], (169, num_child))
-regret = np.reshape(node.regret[:(169*num_child)], (169,num_child))
+seq1 = np.array([0, 0, 0, 0, 0])
+seq2 = np.array([0, 0, 0, 0, 0])
+
+util_sb = util_bb = 0
+start_time = time.time()
+for i in range(1,1000000):
+  mc1, mc2, oc1, oc2, bc1, bc2, bc3, bc4, bc5 = np.random.choice(52, 9, replace=False)
+  seq1[0] = evaluator_cy.preflop_idx(mc1, mc2)
+  seq2[0] = evaluator_cy.preflop_idx(oc1, oc2)
+  seq1[1], _, _  = evaluator.evaluate_flop(mc1, mc2, bc1, bc2, bc3)
+  seq2[1], _, _  = evaluator.evaluate_flop(oc1, oc2, bc1, bc2, bc3)
+  seq1[2], _, _  = evaluator.evaluate_turn(mc1, mc2, bc1, bc2, bc3, bc4)
+  seq2[2], _, _  = evaluator.evaluate_turn(oc1, oc2, bc1, bc2, bc3, bc4)
+  seq1[3], _ = evaluator.evaluate_river(mc1, mc2, bc1, bc2, bc3, bc4, bc5)
+  seq2[3], _ = evaluator.evaluate_river(oc1, oc2, bc1, bc2, bc3, bc4, bc5)
+  seq1[4] = evaluator.evaluate_cards(mc1, mc2, bc1, bc2, bc3, bc4, bc4)
+  seq2[4] = evaluator.evaluate_cards(oc1, oc2, bc1, bc2, bc3, bc4, bc4)
+  util_sb_, util_bb_ = root.run_cfr(seq1, seq2)
+  util_sb += util_sb_
+  util_bb += util_bb_
+  if time.time() - start_time > 0.1:
+    print i, util_sb/i, util_bb/i
+    start_time = time.time()
+
+n = 3
+result = np.reshape(normalize(root.child_nodes[0].average_prob[:(n*169)], n=n), (169,n))
 for i in range(51):
   for j in range(i+1, 52):
     idx = evaluator_cy.preflop_idx(i, j)
-    print util.n2c([i,j]), idx, prob[idx]/np.sum(prob[idx])#, bb_regret[idx]
+    print util.n2c([i,j]), idx, result[idx]
 
 
-
-reload(cfr_cy4)
-from study.cfr import cfr_cy4, cfr_cy2
-root = cfr_cy2.RoundNode(0, 0, 300, 300)
-root2 = cfr_cy4.RoundNode(0, 0, 300, 300)
+reload(cfr_cy3)
+root = cfr_cy3.RoundNode(0, 0, 4, 4)
+root.initialize_regret()
+root.load_buckets()
+board_cards = np.random.choice(52, 5, replace=False)
+timeit root.public_chance_sampling(board_cards)
